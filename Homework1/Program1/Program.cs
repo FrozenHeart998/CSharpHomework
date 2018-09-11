6b29be996ee36b7636ce3b83f33c84136614f918
笛 吴 <wu1352368192@qq.com>
2018-09-11 09:14:50 +0800

    第一次作业

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static private string NumberOneStr;
+        static private string NumberTwoStr;
+
+        static void Main(string[] args)
+        {
+            InputNumber();
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+
+        static private void InputNumber()
+        {
+            Console.WriteLine("请输入第一个数字：");
+            NumberOneStr = Console.ReadLine();
+            Console.WriteLine("请输入第二个数字：");
+            NumberTwoStr = Console.ReadLine();
+
+            try
+            {
+                float NumberOne = float.Parse(NumberOneStr);
+                float NumberTwo = float.Parse(NumberTwoStr);
+                Console.WriteLine("两个数字的乘积是：" + (NumberOne * NumberTwo));
+            }
+            catch
+            {
+                Console.WriteLine("输入有误，请重新输入。");
+                InputNumber();
+            }
+        }
+    }
+}
---
+++ Homework1/Program2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs eventArgs)
+        {
+            try
+            {
+                string NumberOneStr = this.textBox1.Text;
+                string NumberTwoStr = this.textBox2.Text;
+                float NumberOne = float.Parse(NumberOneStr);
+                float NumberTwo = float.Parse(NumberTwoStr);
+                this.label1.Text = "计算结果为：" + (NumberOne * NumberTwo);
+            }
+            catch
+            {
+                this.label1.Text = "输入有误，请重新输入。";
+            }
+        }
+    }
+}
