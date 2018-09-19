5b923a10be1bb337754979fd576608ac85b289b2
笛 吴 <wu1352368192@qq.com>
2018-09-19 13:02:20 +0800

    第二次作业

--- Homework2/Program1/Program.cs
+++ Homework2/Program1/Program.cs
@@ -7,64 +7,50 @@ namespace Program1
     {
         static void Main(string[] args)
         {
-            int[] anArray = InData();
-            OutPrimeNum(anArray);
+            PrimeFactor();
             Console.WriteLine("按任意键退出...");
             Console.ReadKey();
         }
 
-        static int[] InData()
+        static void PrimeFactor()
         {
-            int aSize = 0;
+            int userNum = 1;
+            int primeFactor = 2;
+
             try
             {
-                Console.WriteLine("请输入你的数据个数(输入0直接退出)：");
-                aSize = int.Parse(Console.ReadLine());
+                Console.WriteLine("请输入一个整数：");
+                userNum = Int32.Parse(Console.ReadLine());
             }
             catch
             {
                 Console.WriteLine("你的输入有误，请重新输入。\n");
-                InData();
+                PrimeFactor();
             }
 
-            if (aSize == 0)
+            Console.Write("该整数的素数因子为：");
+
+            if (userNum == 1)
             {
-                System.Environment.Exit(0);
+                Console.WriteLine(userNum);
             }
-
-            int[] anArray = new int[aSize];
-
-            Console.WriteLine("请依次输入你的数据：");
-
-            for (int i = 0; i < aSize; i++)
+            else
             {
-
-                try
+                while (primeFactor <= userNum)
                 {
-                    anArray[i] = int.Parse(Console.ReadLine());
+                    if (userNum % primeFactor == 0)
+                    {
+                        Console.Write(primeFactor + " ");
+                        userNum /= primeFactor;
+                    }
+                    else
+                    {
+                        primeFactor++;
+                    }
                 }
-                catch
-                {
-                    Console.WriteLine("你的输入有误，请重新输入。\n");
-                    i--;
-                }
+                Console.WriteLine();
             }
-
-            return anArray;
-
-        }
-
-        static void OutPrimeNum(int[] anArray)
-        {
-            Console.WriteLine("\n你输入的数据中是素数的有：");
-            foreach (int aNum in anArray)
-            {
-                if (aNum % 2 != 0 && aNum % 3 != 0 && aNum % 5 != 0 && aNum % 7 != 0
-                    || aNum == 2 || aNum == 3 || aNum == 5 || aNum == 7)
-                {
-                    Console.WriteLine("" + aNum);
-                }
-            }
+            
         }
     }
 }
